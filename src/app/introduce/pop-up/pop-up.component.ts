d84e38a3e4c7e3b67d83ed782abaaa977575091e
CHI_BUI <chibv@fpt.com.vn>
2022-10-19 00:47:45 +0700

    chibv

---
+++ src/app/introduce/pop-up/pop-up.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up',
+  templateUrl: './pop-up.component.html',
+  styleUrls: ['./pop-up.component.scss']
+})
+export class PopUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  closeNotiPopup() {
+    alert("xin chaof")
+  }
+
+}
